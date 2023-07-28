50131e56f1ad54b7c0f7cdf94a0a687b3d6db0c6
Rohit Saini <rs0118032@gmail.com>
2023-07-28 15:33:29 +0530

    deployment changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-//connect to database
-mongoose.connect('mongodb://127.0.0.1/PlacementCell');
+// //connect to database
+// mongoose.connect('mongodb://127.0.0.1/PlacementCell');
+
+mongoose.connect("mongodb+srv://rrs0118032:J8bcv7VSepMH2DXI@cluster0.w03xxyy.mongodb.net/placementcell");
 
 //acquire the connection(to check if it is successful)
 const db=mongoose.connection;
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const path = require('path');
-const port = 8000;
-
-const app = express();
-
-const db = require('./config/mongoose');
-
-// used for session cookie
+// const port = 8000;
+const PORT = process.env.PORT || 8000;
+const dotenv = require('dotenv');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
 
+// require('dotenv').config();
+dotenv.config({ path: 'config/.env' });
+
+const app = express();
+
+const db = require('./config/mongoose');
+
+
+
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
+
+// used for session cookie
 app.use(session({ 
     name:'placement cell app',
     secret:'hehe',
@@ -42,9 +49,9 @@ app.use(express.urlencoded());
 //use express router
 app.use('/',require('./routes'));
 
-app.listen(port, function(err){
+app.listen(PORT, function(err){
     if(err){
         console.log(`Error :${err}`);
     }
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
